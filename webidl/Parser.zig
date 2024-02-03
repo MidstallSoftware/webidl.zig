664d1d0bf41f9afee8345fe9d38bb9c5e7a9d215
Tristan Ross <tristan.ross@midstall.com>
2024-02-02 16:00:03 -0800

    fix(parser): integer test failing

--- webidl/Parser.zig
+++ webidl/Parser.zig
@@ -33,7 +33,7 @@ fn identifier(input: []const u8) usize {
     const first_char = "-_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const all_chars = first_char ++ "0123456789";
     for (input, 0..) |c, i| {
-        if (std.mem.indexOfScalar(u8, if (i > 0) all_chars else first_char, c) == null) {
+        if (std.mem.indexOfScalar(u8, if (i > 0 and input[0] != '-') all_chars else first_char, c) == null) {
             return i;
         }
     }
@@ -54,6 +54,15 @@ pub const Tokenizer = ptk.Tokenizer(TokenType, &[_]Pattern{
     Pattern.create(.identifier, struct {
         fn func(input: []const u8) ?usize {
             const i = identifier(input);
+            if (input[0] == '-') {
+                var digits: usize = 0;
+                for (input[1..i]) |ch| {
+                    if (std.ascii.isDigit(ch)) {
+                        digits += 1;
+                    }
+                }
+                if (digits == (i - 1)) return null;
+            }
             return if (std.meta.stringToEnum(productions.Symbol.Type, input[0..i])) |_| null else i;
         }
     }.func),
